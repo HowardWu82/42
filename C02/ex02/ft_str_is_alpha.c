7e7f0cd146946a50a408c1563da37d0cff148d66
Hao Wu <hawu@F2N062.42adel.org.au>
2025-02-10 17:06:52 +1030

    progress on ex02

--- C02/ex02/ft_str_is_alpha.c
+++ C02/ex02/ft_str_is_alpha.c
@@ -6,7 +6,7 @@
 /*   By: hawu <hawu@student hawu@student.42ade      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 14:31:05 by hawu              #+#    #+#             */
-/*   Updated: 2025/02/10 15:26:05 by hawu             ###   ########.fr       */
+/*   Updated: 2025/02/10 17:01:57 by hawu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stdio.h>
@@ -17,30 +17,32 @@ int	ft_str_is_alpha(char *str)
 	int	start;
 	int	yesorno; 
 
-	start = '0';
-	
-	while (start < '\0')
+	start = 0;
+	yesorno = 1;
+	while (str[start] != '\0')
 	{
-		if (str[start] >= 'A' && str[start] <= 'Z' || str[start] >= 'a' && str[start] <= 'z' || str[start] = '\0')
+		if ((str[start] >= 'A' && str[start] <= 'Z') || (str[start] >= 'a' && str[start] <= 'z'))
 		{
-			yesorno = '1';
+			yesorno = 1;
 			write(1, &yesorno, 1);
+			start++;
 		}
 
 		else
 		{
-			yesorno = '0';
+			yesorno = 0;
 			write(1, &yesorno, 1);
+			start++;
 		}
 	}
-	return(&yesorno)
+	return &yesorno;
 }
 
-int	main(yesorno)
+int	main(void)
 {
 	char	str[]="abcdefg";
 
 	ft_str_is_alpha(str);
-	printf("answer = %d", yesorno);
+	printf("answer = %d", &yesorno);
 	return(0);
 }
